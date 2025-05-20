16cf20a9068c8fbdd46270eb81d582b7d92c9c94
H4unt3r564 <jasiekpraga@gmail.com>
2025-05-20 14:28:23 +0200

    Add files via upload
    zrobiony main prawie

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// SLIDER
 obrazki = document.querySelectorAll(".obrazek")
 
 obrazki[0].className = "obrazekAktywny"
@@ -19,3 +20,55 @@ function zmianaObrazka(kierunek){
     obrazki[nowyIndex].className = "obrazekAktywny";
     obrazki[nowyIndex].style.animationName = kierunek === "lewo" ? "przewijanieZLewej" : "przewijanieZPrawej";
 }
+
+
+// MENU ROZWIJANE
+const menu = document.getElementById("menuRozwijane");
+
+function menuWysuwane() {
+    const widokTelefon = window.matchMedia("(max-width: 768px)").matches;
+    if(widokTelefon){
+        menu.classList.toggle("pokazaneMenu");
+        menu.classList.toggle("ukryteMenu");
+    }       
+}
+
+window.addEventListener("resize", () =>{
+    const widokTelefon = window.matchMedia("(max-width: 768px)").matches; //musi byc dwa razy bo sie aktualizuje
+        if(!widokTelefon){
+            menu.classList.remove("pokazaneMenu");
+            menu.classList.add("ukryteMenu");
+        }
+})
+
+// MODALE
+modal = document.getElementById("modal");
+modalZdjecie = document.getElementById("modalZdjecie");
+modalImie = document.getElementById("modalImie");
+modalWiek = document.getElementById("modalWiek");
+modalOpis = document.getElementById("modalOpis");
+guzikX = document.getElementById("guzikX");
+
+zwierzaki = document.querySelectorAll(".row img");
+
+zwierzaki.forEach(img => {
+    img.addEventListener("click", () => {
+        modalImie.innerHTML = "imie";
+        modalWiek.innerHTML = "wiek";
+        modalOpis.innerHTML = "opis";
+        modal.style.display = "block";
+    })
+});
+
+function zamknijModal(){
+    modal.style.display = "none";
+}
+
+// DARkMODE
+guzikDarkmode = document.querySelectorAll(".guzikDarkmode");
+
+guzikDarkmode.forEach(guzik =>
+    guzik.addEventListener("click", () => {
+        document.querySelector("body").classList.toggle("dark-mode");
+    })
+)
