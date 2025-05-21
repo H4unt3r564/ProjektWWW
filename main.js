4fffb89c6412a6b3b359e38c9485de539e1a0f9f
H4unt3r564 <jasiekpraga@gmail.com>
2025-05-21 09:43:01 +0200

    Add files via upload
    js i nowe logo

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // SLIDER
-obrazki = document.querySelectorAll(".obrazek")
-
+const obrazki = document.querySelectorAll(".obrazek")
 obrazki[0].className = "obrazekAktywny"
 
 function zmianaObrazka(kierunek){
@@ -23,21 +22,33 @@ function zmianaObrazka(kierunek){
 
 
 // MENU ROZWIJANE
+const menu = document.getElementById("menuRozwijane");
+
 function menuWysuwane() {
-    const menu = document.getElementById("menuRozwijane");
-    menu.classList.toggle("pokazaneMenu");
-    menu.classList.toggle("ukryteMenu");
+    const widokTelefon = window.matchMedia("(max-width: 768px)").matches;
+    if(widokTelefon){
+        menu.classList.toggle("pokazaneMenu");
+        menu.classList.toggle("ukryteMenu");
+    }       
 }
 
-// MODALE
-modal = document.getElementById("modal");
-modalZdjecie = document.getElementById("modalZdjecie");
-modalImie = document.getElementById("modalImie");
-modalWiek = document.getElementById("modalWiek");
-modalOpis = document.getElementById("modalOpis");
-guzikX = document.getElementById("guzikX");
+window.addEventListener("resize", () =>{
+    const widokTelefon = window.matchMedia("(max-width: 768px)").matches; //musi byc dwa razy bo sie aktualizuje
+        if(!widokTelefon){
+            menu.classList.remove("pokazaneMenu");
+            menu.classList.add("ukryteMenu");
+        }
+})
 
-zwierzaki = document.querySelectorAll(".row img");
+// MODALE
+const modal = document.getElementById("modal");
+const modalZdjecie = document.getElementById("modalZdjecie");
+const modalImie = document.getElementById("modalImie");
+const modalWiek = document.getElementById("modalWiek");
+const modalOpis = document.getElementById("modalOpis");
+const guzikX = document.getElementById("guzikX");
+
+const zwierzaki = document.querySelectorAll(".row img");
 
 zwierzaki.forEach(img => {
     img.addEventListener("click", () => {
@@ -50,4 +61,31 @@ zwierzaki.forEach(img => {
 
 function zamknijModal(){
     modal.style.display = "none";
+}
+
+// DARkMODE
+const guzikDarkmode = document.querySelectorAll(".guzikDarkmode");
+const logo = document.getElementById("logo").querySelector("img");
+
+guzikDarkmode.forEach(guzik =>
+    guzik.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+        czyDark = document.body.classList.contains("dark-mode");
+
+        //zmiana logo
+        logo.src = czyDark ? "nowelogo_darkmode.jpg" : "logo.jpg";
+        //localstorage na darkmode
+        localStorage.setItem("darkmode", czyDark);
+    })
+)
+
+//LOCALSTORAGE
+window.onload = function(){
+    if(localStorage.getItem("darkmode") === "true"){
+        document.body.classList.add("dark-mode");
+        logo.src = "nowelogo_darkmode.jpg";
+    }else{
+        document.body.classList.remove("dark-mode");
+        logo.src = "logo.jpg";
+    }
 }
